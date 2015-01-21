4cf00f0de213a3c25040e6acd12981bb0c4d7f67
truongld <mr.truongld@gmail.com>
2015-01-21 15:36:23 +0700

    Update app.js

--- templates/javascript/app.js
+++ templates/javascript/app.js
@@ -4,18 +4,33 @@ define(['angular']/*deps*/, function (angular)/*invoke*/ {
   'use strict';
 
   var app = angular.module('<%= scriptAppName %>', [/*angJSDeps*/<%= angularModules %>])<% if (ngRoute) { %>
-    .config(function ($routeProvider, $controllerProvider, $provide, $locationProvide) {
-      
-      //$locationProvider.html5Mode(true).hashPrefix('!');
-      
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          resolve: resolveController(['controllers/main'])
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-    })<% } %>;
+    .config(
+    [
+      '$routeProvider',
+      '$locationProvider',
+      '$controllerProvider',
+      '$compileProvider',
+      '$filterProvider',
+      '$provide',
+      function ($routeProvider, $locationProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
+	      
+	      app.controller = $controllerProvider.register;
+	      app.directive  = $compileProvider.directive;
+	      app.filter     = $filterProvider.register;
+	      app.factory    = $provide.factory;
+	      app.service    = $provide.service;
+	      
+	      //$locationProvider.html5Mode(true).hashPrefix('!');
+	      
+	      $routeProvider
+	        .when('/', {
+	          templateUrl: 'views/main.html',
+	          resolve: resolveController(['controllers/main'])
+	        })
+	        .otherwise({
+	          redirectTo: '/'
+	        });
+    	}
+    ])<% } %>;
 	return app;
 });
